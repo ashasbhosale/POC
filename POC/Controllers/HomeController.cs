98fdc6439e866cf84a1812bd17190466d06ecff6
ashasbhosale <ashabhosale009@Gmail.com>
2019-01-04 04:12:22 -0600

    test2

--- POC/Controllers/HomeController.cs
+++ POC/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace POC.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Jenkins CI POC Test 1 on AWS";
+            ViewBag.Message = "Jenkins CI POC Test 1 on AWS1";
 
             return View();
         }
